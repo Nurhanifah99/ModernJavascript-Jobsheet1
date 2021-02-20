6c0236eeb6e214b4fecf7aa706482aa874d78c91
Nurhanifah99 <Hanifah110498@gmail.com>
2021-02-20 14:49:50 +0700

    first

---
+++ arrow.js
@@ -0,0 +1,9 @@
+//Tampa Arrow
+//function gretting(message){
+//retrun alert('${message} Cantik !')
+//}
+
+//Menggunakan Arrow
+let gretting = message => alert(`${message} Cantik !`);
+
+gretting('Selamat Pagi');
---
+++ arrowf.js
@@ -0,0 +1,11 @@
+//tanpa arrow
+//cost func = function(a,b){
+// retrun a+b;
+//};
+//alert(func(3, 3));
+
+//menggunakan arrow
+const func = (a, b) => {
+    return a + b;
+};
+alert(func(5, 4));
